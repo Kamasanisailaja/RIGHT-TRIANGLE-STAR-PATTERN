92662991643f84a7252e11ea3bafc6db12bf6a43
Kamasanisailaja <106640335+Kamasanisailaja@users.noreply.github.com>
2023-05-20 13:08:58 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,21 @@
+//right angle triangle pattern
+
+import java.util.Scanner;
+class Pattern
+{
+  public static void main(String[] args)
+  {
+     int i,j,n;
+     Scanner in = new Scanner(System.in);
+     System.out.println("enter the value of n:");
+     n=in.nextInt();
+     for(i=1;i<=n;i++)
+     {
+       for(j=1;j<=i;j++)
+       {
+         System.out.print("*");
+       }
+       System.out.println();
+     }
+  }
+}
